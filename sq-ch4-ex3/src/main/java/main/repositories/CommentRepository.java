9ec3ce6b49c6dd5faa98509169617e1dac0eab29
Ignacio <casaresignacio17@gmail.com>
2025-04-01 20:23:43 -0300

    Adding sq-ch4-ex3, Abstraction with DI and Spring Implementation, using Autowired

---
+++ sq-ch4-ex3/src/main/java/main/repositories/CommentRepository.java
@@ -0,0 +1,7 @@
+package main.repositories;
+
+import main.model.Comment;
+
+public interface CommentRepository {
+    void storeComment(Comment comment);
+}
---
+++ sq-ch4-ex3/src/main/java/main/repositories/DBCommentRepository.java
@@ -0,0 +1,13 @@
+package main.repositories;
+
+import main.model.Comment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DBCommentRepository implements CommentRepository {
+
+    @Override
+    public void storeComment(Comment comment) {
+        System.out.println("Storing comment: " + comment.getText());
+    }
+}
