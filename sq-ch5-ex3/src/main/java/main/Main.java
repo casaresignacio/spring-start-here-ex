ac4abdd9a3465716e795f5ef4dcd082e9ba93484
Ignacio <casaresignacio17@gmail.com>
2025-04-13 11:18:37 -0300

    Adding sq-ch5-ex3 example, eager instance example

---
+++ sq-ch5-ex3/src/main/java/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import main.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        //This app creates the Spring context, but it doesn't use the CommentService bean anywhere
+        var c = new AnnotationConfigApplicationContext(ProjectConfig.class);
+    }
+}
---
+++ sq-ch5-ex3/src/main/java/main/services/CommentService.java
@@ -0,0 +1,11 @@
+package main.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+
+    public CommentService() {
+        System.out.println("CommentService isntance created!");
+    }
+}
